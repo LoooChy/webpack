bb1dca0f4fa4e15da04c4ffd3064016deb050c28
loochy <qingyi.lu@jfhealthcare.com>
2021-08-17 17:09:24 +0800

    新增功能

--- webpack/src/js/lib/L3D.js
+++ webpack/src/js/lib/L3D.js
@@ -7,7 +7,7 @@
  * if you want create a small 3d project,and you has not basic for 3d,then L3D  probably let you come in 3D world.
  * 
  * you must need three javascript files:
- * L3D.js , L.js , three.js
+ * L3D.js, three.js
  * and other javascript files on my github ,if you need ,you can download.
  * 
  * my gitgub : https://github.com/LoooChy
@@ -15,6 +15,10 @@
  */
 // import L from './L.js';
 import * as THREE from './three.module.js';
+import Stats from '../jsm/libs/stats.module.js';
+import { Line2 } from '../jsm/lines/Line2.js';
+import { LineMaterial } from '../jsm/lines/LineMaterial.js';
+import { LineGeometry } from '../jsm/lines/LineGeometry.js';
 import { OrbitControls } from '../jsm/controls/OrbitControls.js';
 class L3D {
     constructor(object) {
@@ -23,18 +27,18 @@ class L3D {
             box: document.querySelector(object.el),//容器
             width: object.viewport[0],//容器宽度
             height: object.viewport[1],//容器高度
-            allMesh: [],
             INTERSECTED: null,
             allObject: object,//初始化时所有的对象
             clock: new THREE.Clock(),//时钟
             controls: null,
-            version: 0.1
+            version: 0.2,
+            pointer: new THREE.Vector2(),
+            raycaster: new THREE.Raycaster(),
+            fps:object.render.fps,
+            stats:new Stats()
         }
         console.log("L3D " + this.params.version + " DEV");
         this.init(object);
-        // console.log(this);
-
-
     }
 
     init = (object) => {
@@ -42,9 +46,10 @@ class L3D {
         this.initCamera(object);
         this.initLight(object);
         this.initAxis(object);
-        this.initObject(object);
-        this.render(object);
+        this.initObjectFn(object);
+        this.render();
         this.bindHandler(object);
+        this.onResize();
     }
 
     initRender = (object) => {
@@ -53,39 +58,100 @@ class L3D {
             alpha: object.render.alpha,
         });
         object.render.alpha ? this.renderer.setClearColor(0xffffff, 0) : this.renderer.setClearColor(0xffffff, 1);
-        // this.renderer.shadowMapEnabled = true;
+        this.renderer.shadowMap.enabled = true;
         this.renderer.setSize(this.params.width, this.params.height);
         document.querySelector(object.el).append(this.renderer.domElement);
+        this.params.fps?document.querySelector(object.el).appendChild(this.params.stats.dom):null;
     }
 
     initCamera = (object) => {
         this.camera = new THREE.PerspectiveCamera(45, this.params.width / this.params.height, 1, 1000);
         this.camera.position.set(...object.camera.position);
-        object.camera.controls ? this.params.controls = new OrbitControls(this.camera, this.renderer.domElement) : this.scene.add(this.camera);
+        object.camera.controls ? this.params.controls = new OrbitControls(this.camera, this.renderer.domElement) : this.add(this.camera);
     }
 
     initLight = (object) => {
+        if (object.light && object.light.length > 0) {
+            object.light.forEach((item) => {
+                switch (item.type) {
+                    case 'AmbientLight':
+                        const ambient = new THREE.AmbientLight(item.color, item.intensity);
+                        ambient.name = item.name;
+                        this.add(ambient);
+                        break;
+                    case 'PointLight':
+                        const pointLight = new THREE.PointLight(item.color, item.intensity, item.distance ? item.distance : 0, item.decay ? item.decay : 1);
+                        pointLight.name = item.name;
+                        pointLight.castShadow = item.shadow ? true : false;
+                        pointLight.position.set(...item.position);
+                        this.add(pointLight);
+                        break;
+                    case 'DirectionalLight':
+                        const directionalLight = new THREE.DirectionalLight(item.color, item.intensity);
+                        directionalLight.name = item.name;
+                        directionalLight.position.set(...item.position);
+                        this.add(directionalLight);
+                        break;
+                    case 'SpotLight':
+                        const spotLight = new THREE.SpotLight(item.color, item.intensity, item.distance, item.angle, item.penumbra ? item.penumbra : 0, item.decay ? item.decay : 1);
+                        spotLight.name = item.name;
+                        spotLight.position.set(...item.position);
+                        spotLight.castShadow = item.shadow ? true : false;
+                        // spotLight.shadow.mapSize.width = 20;
+                        // spotLight.shadow.mapSize.height = 20;
+                        // spotLight.shadow.camera.near = 20;
+                        // spotLight.shadow.camera.far = 100;
+                        // spotLight.shadow.camera.fov = 30;
+                        const spotLightHelper = new THREE.SpotLightHelper(spotLight);
 
-    }
+                        this.add(spotLight);
+                        item.help ? this.add(spotLightHelper) : null;
+                        break;
+                    case 'RectAreaLight':
+                        const rectAreaLight = new THREE.RectAreaLight(item.color, item.intensity, item.width, item.height);
+                        rectAreaLight.name = item.name;
+                        rectAreaLight.position.set(...item.position);
+                        rectAreaLight.lookAt(...item.lookAt);
+                        this.add(rectAreaLight);
+                        break;
+                }
+            })
 
-    initAxis = (object) => {
-        if (object.axis.isOpen) {
-            var axes = new THREE.AxisHelper(object.axis.length);
-            this.scene.add(axes)
         }
     }
 
-    initObject = (object) => {
-        object.initObject && object.initObject(this, THREE)
+    initAxis = (object) => {
+        const { axis } = object;
+        if (axis.isOpen) {
+            const axes = new THREE.AxesHelper(axis.length);
+            const grid = new THREE.GridHelper(axis.gridNum[0], axis.gridNum[1], 0x303030, 0x303030);
+            this.add(axes);
+            this.add(grid);
+        }
     }
 
-    render = (object) => {
+    initObjectFn = (object) => {
+        object.initObjectFn && object.initObjectFn(this, THREE);
+    }
+
+    render = () => {
         this.animate(this);
-        requestAnimationFrame(this.render)
+        requestAnimationFrame(this.render);
     }
 
-    animate = (object) => {
-        this.renderer.render(this.scene, this.camera)
+    animate = () => {
+        this.params.allObject.animateFn && this.params.allObject.animateFn(this)
+        this.params.raycaster.setFromCamera(this.params.pointer, this.camera);
+        const intersects = this.params.raycaster.intersectObjects(this.scene.children);
+        this.params.fps?this.params.stats.update():null;
+        if (intersects.length > 0) {
+            if (this.params.INTERSECTED != intersects[0].object) {
+                this.params.INTERSECTED = intersects[0].object;
+            }
+        } else {
+            this.params.INTERSECTED = null;
+        }
+        this.renderer.render(this.scene, this.camera);
     }
 
     loaderModle = (object) => {
@@ -97,37 +163,116 @@ class L3D {
     }
 
     bindHandler = (object) => {
-
-    }
-
-    onResize = (object) => {
-
-    }
-
-    ray = (object) => {
-
-    }
-
-    creatPlanMessage = (object) => {
-
-    }
-
-    createCube = (obj) => {
-        const geometry = new THREE.BoxGeometry(obj.attrbuilt);
-        const material = new THREE.MeshBasicMaterial({ 
-            // map: texture 
-            color:obj.color
+        document.addEventListener('pointermove', (event) => {
+            this.params.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+            this.params.pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
         });
-        const mesh = new THREE.Mesh( geometry, material )
-        mesh.name = obj.name
-        // mesh.position.set(0,0,0)
-        mesh.computeBoundingSphere()
-        return mesh
+        object.bindHandlerFn && object.bindHandlerFn(this);
     }
 
-    createSpehre = (object) => {
+    onResize = () => {
+        window.addEventListener('resize', () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.renderer.render(this.scene, this.camera);
+        });
+    }
 
+    create3Dobj = (obj) => {
+        const { base, position, name, objType, materialObj, castShadow, receiveShadow } = obj;
+        const { materiaType, color, wireframe, opacity, transparent, texture, size, fog, linewidth } = materialObj;
+        let geometry, material;
+        switch (objType) {
+            case 'cube':
+                geometry = new THREE.BoxGeometry(...base);
+                break;
+            case 'spehre':
+                geometry = new THREE.SphereGeometry(...base);
+                break;
+            case 'plane':
+                geometry = new THREE.PlaneGeometry(...base);
+                break;
+            case 'point':
+                geometry = this.getPoint(position, color);
+                break;
+            case 'line':
+                geometry = new LineGeometry();
+                geometry.setPositions(base);
+                break;
+            default:
+                geometry = new THREE.BoxGeometry(10, 10, 10);
+        }
+        switch (materiaType) {
+            case 'basic':
+                material = new THREE.MeshBasicMaterial({ map: texture ? texture[0] : undefined, color: color, wireframe: wireframe ? true : false });
+                break;
+            case 'phong':
+                material = new THREE.MeshPhongMaterial({ map: texture ? texture[0] : undefined, color: color, shininess: 100, opacity: opacity, transparent: transparent, wireframe: wireframe ? true : false });
+                break;
+            case 'lambert':
+                material = new THREE.MeshLambertMaterial({ map: texture ? texture[0] : undefined, color: color, opacity: opacity, transparent: transparent, wireframe: wireframe ? true : false });
+                break;
+            case 'particle':
+                material = new THREE.PointsMaterial({ map: texture ? texture[0] : undefined, color: color, opacity: opacity, transparent: transparent, size: size });
+                break;
+            case 'shadow':
+                material = new THREE.ShadowMaterial({ opacity: opacity });//接受阴影，但在其他地方完全透明；
+                break;
+            case 'sprite':
+                material = new THREE.SpriteMaterial({ map: texture ? texture[0] : undefined, color: color, fog: fog });
+                break;
+            case 'line':
+                material = new LineMaterial({ color: color, linewidth: linewidth, opacity: opacity, transparent: transparent });
+                material.resolution.set(window.innerWidth, window.innerHeight)
+                break;
+            default:
+                material = new THREE.MeshNormalMaterial();
+        }
+        let mesh;
+        if (objType === 'point') {
+            mesh = new THREE.Points(geometry, material);
+        } else if (objType === 'sprite') {
+            mesh = new THREE.Sprite(material);
+            mesh.scale.set(...base);
+        } else if (objType === 'line') {
+            console.log(geometry, "geometrygeometry")
+            mesh = new Line2(geometry, material);
+        } else {
+            mesh = new THREE.Mesh(geometry, material);
+        }
+        mesh.name = name;
+        mesh.castShadow = castShadow ? true : false;
+        mesh.receiveShadow = receiveShadow ? true : false;
+        mesh.position.set(...position);
+        return mesh;
+    }
+
+    getGeometry = (name) => {
+        const geometry = name ? this.scene.getObjectByName(name) : '';
+        return geometry;
+    }
+    loadTexture = () => {
+        return new THREE.TextureLoader();
+    }
+    getCurrentObj = () => {
+        return this.params.INTERSECTED;
+    }
+    getPoint = (position, color) => {
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+        geometry.setAttribute('color', new THREE.Float32BufferAttribute(new THREE.Color(color), 3));
+        geometry.computeBoundingSphere();
+        return geometry;
+    }
+    add = (obj) => {
+        this.scene.add(obj);
+    }
+    getAllObj = () => {
+        return this.scene.children;
     }
 }
 
 export default L3D
+
+// TODO加载模型
--- webpack/src/js/test_model.js
+++ webpack/src/js/test_model.js
@@ -1,34 +1,184 @@
 
 import L3D from './lib/L3D.js';
-import { Color } from './lib/three.module.js';
-// import L from './lib/L.js';
+import picture1 from '../images/brick_diffuse.jpg';
+import picture2 from '../images/hardwood2_diffuse.jpg';
+
 
 const a = new L3D({
     el: "#canvas-frame",//容器ID
     viewport: [
-        document.querySelector("#canvas-frame").offsetWidth, 
+        document.querySelector("#canvas-frame").offsetWidth,
         document.querySelector("#canvas-frame").offsetHeight
     ],//容器宽高
     render: {
         antialias: true,//抗锯齿
-        alpha: false,//透明度
+        alpha: true,//开启透明
+        fps:false//开启帧数检测
     },
-    camera:{
-        position:[0,240,350],//相机坐标
-        controls:true//开启控制
+    camera: {
+        position: [150, 440, 450],//相机坐标
+        controls: true//开启控制
     },
-    axis:{
-        isOpen:true,//坐标轴
-        length:200//坐标轴长度
+    axis: {
+        isOpen: true,//坐标轴
+        length: 200,//坐标轴长度
+        gridNum: [200, 40],//【网格大小,网格密度】
     },
-    initObject:(L,TH)=>{
-        const cube = L.createCube({
-            attrbuilt:[20,20,20],
-            color:'#ff00ff',
-            name:'cube1'
+    light: [
+        {
+            type: 'DirectionalLight',
+            color: '',
+            position: [],
+            intensity: 0.9
+        },
+        {
+            type: 'PointLight',
+            name: 'pointLight',
+            color: '#ffffff',
+            shadow: false,
+            position: [40, 90, 40],
+            intensity: 1,//光照强度
+            distance: 0,//光源到光照强度为0的位置
+            decay: 1//沿着光照距离的衰退量
+        },
+        {
+            type: 'AmbientLight',
+            name: 'ambientLight',
+            color: '#ffffff',
+            intensity: 0.9
+        },
+        {
+            type: 'SpotLight',
+            color: '#00ff00',
+            shadow: true,
+            position: [100, 200, 100],
+            intensity: 0.9,
+            distance: 300,//从光源发出光的最大距离，其强度根据光源的距离线性衰减
+            angle: Math.PI / 8,//光线散射角度，最大为Math.PI/2
+            penumbra: 0,//聚光锥的半影衰减百分比。在0和1之间的值。默认为0。
+            decay: 1,//沿着光照距离的衰减量
+            help:true
+        },
+        {
+            type: 'RectAreaLight',
+            name: 'rectAreaLight',
+            color: '#ffffff',
+            intensity: 0.9,
+            width: 30,
+            height: 70,
+            position: [-30, 0, -60],
+            lookAt: [0, 0, 0]//面光源朝向
+        }
+    ],
+    initObjectFn: (L) => {
+        const loader = L.loadTexture();
+        const pic1 = loader.load(picture1);
+        const pic2 = loader.load(picture2);
+
+        const cube = L.create3Dobj({
+            objType: 'cube',
+            base: [20, 20, 20],
+            position: [0, 0, 0],
+            name: 'cube1',
+            materialObj: {
+                materiaType: 'basic',
+                color: '#ff00ff',
+                wireframe: false,
+                texture: [pic1]
+            }
         });
-        L.scene.add(cube)
+        L.add(cube);
+        const spehre = L.create3Dobj({
+            objType: 'spehre',
+            base: [20],
+            position: [30, 30, 30],
+            name: 'spehre1',
+            castShadow:true,
+            materialObj: {
+                materiaType: 'phong',
+                color: '#f0f0f0',
+                wireframe: false,
+                opacity: 0.9,
+                transparent: false,
+                texture: [pic2],
+            }
+        });
+        L.add(spehre);
+        const plane = L.create3Dobj({
+            objType: 'plane',
+            base: [180, 180],
+            position: [0, 0, 0],
+            name: 'plane1',
+            receiveShadow:true,
+            materialObj: {
+                materiaType: 'shadow',
+                color: '#ff00ff',
+                wireframe: false,
+                opacity: 0.2,
+                transparent: false,
+                // texture: [pic2]
+            }
+        });
+        plane.rotateX(-Math.PI * 0.5);
+        L.add(plane);
+
+        const points = L.create3Dobj({
+            objType: 'point',
+            position: [50, 50, 50],
+            name: 'point1',
+            materialObj: {
+                materiaType: 'particle',
+                color: '#334477',
+                opacity: 0.9,
+                transparent: false,
+                // texture: [pic2],
+                size:15,//点的大小
+            }
+        });
+        L.add(points);
+
+        const sprite = L.create3Dobj({
+            objType: 'sprite',
+            base:[100,50,1],
+            position: [-50, -50, -50],
+            name: 'sprite1',
+            fog:true,
+            materialObj: {
+                materiaType: 'sprite',
+                color: '#ffffff',
+                opacity: 0.9,
+                transparent: false,
+                texture: [pic1],
+                // size:15,//点的大小
+            }
+        });
+        L.add(sprite);
+
+        const line = L.create3Dobj({
+            objType: 'line',
+            base:[0,0,0,100,50,1],
+            position: [-50, 20, 10],
+            name: 'line1',
+            materialObj: {
+                materiaType: 'line',
+                color: '#ff00ff',
+                opacity: 0.5,
+                transparent: true,
+                linewidth:5
+            }
+        });
+        L.add(line);
+
+    },
+    animateFn: (L) => {
+        const spehre1 = L.getGeometry('spehre1');
+        const elapsed = L.params.clock.getElapsedTime();
+        spehre1.position.set(Math.sin(elapsed) * 5, 30, Math.cos(elapsed) * 5);
+    },
+    bindHandlerFn: (L) => {
+        document.addEventListener('click', () => {
+            console.log(L.getCurrentObj(),"currentObj");
+            console.log(L.getAllObj(),"getAllObj");
+        });
     }
 })
-
-console.log()
